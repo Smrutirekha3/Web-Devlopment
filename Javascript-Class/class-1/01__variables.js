f88058e41b39191f5cd9ed87d5b8b2718e4ff923
Smrutirekha3 <smrutirekhas248@gmail.com>
2024-05-16 23:46:33 +0530

    new files added

---
+++ Javascript-Class/class-1/01__variables.js
@@ -0,0 +1,21 @@
+let num=100;
+console.log(num);// decalre a variable
+
+let Num = 100;
+console.log(num); // to print the output
+
+
+
+// space dosn't allow while declaring variable
+// let second num = 1000;
+
+let second_num = 1000; // second_num(snakeCase)
+console.log(second_num);
+
+let secondNum = 1000; // secondNum(camelCase)
+console.log(secondNum);
+
+let decimal_num = 15.35;
+console.log(decimal_num);
+
+console.log(typeof decimal_num);
